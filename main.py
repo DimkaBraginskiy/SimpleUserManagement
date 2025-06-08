8b743e407dc5769732b944bef0c1b264ce013292
Dmytro Brahinskyi <dbraginskiy821@gmail.com>
2025-06-08 16:36:35 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+# class Use(object):
+# def __init__(self, username, name, email, phone, age):
+#     self.username = username
+#     self.name = name
+#     self.email = email
+#     self.phone = phone
+#     self.age = age
+#
+
+
+
+
+
+def init():
+    while True:
+        userinput = input("--- User CRUD Menu ---\n"
+                            "1. Create User\n"
+                            "2. View User\n"
+                            "3. Update User\n"
+                            "4. Delete User\n"  
+                            "5. Show All Users\n"
+                            "0. Exit\n"
+                          "Your input: ")
+
+        match userinput:
+
+
+
+if __name__ == "__main__":
+    init()
